7512078ee109f234c534847fb43ea444c8305972
Dima <pleskunindimooon@gmail.com>
2025-08-24 11:03:11 +0300

    add [...all].js

--- uimapp/server/api/index.js
+++
@@ -1,6 +0,0 @@
-// uimapp/server/api/index.js
-const app = require('../src/app');
-
-// Express-приложение само является (req,res)-handler'ом.
-// Явно оборачиваем — так надёжнее для Vercel:
-module.exports = (req, res) => app(req, res);
